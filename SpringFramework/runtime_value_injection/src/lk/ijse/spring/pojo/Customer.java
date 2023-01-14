dd94dfb216059d721d6a69c41f8812deedd6e596
mGunawardhana <mrgunawardhana27368@gmail.com>
2023-01-14 23:02:20 +0530

    content update

--- SpringFramework/runtime_value_injection/src/lk/ijse/spring/pojo/Customer.java
+++ SpringFramework/runtime_value_injection/src/lk/ijse/spring/pojo/Customer.java
@@ -6,12 +6,12 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Customer {
-//    public Customer(@Value("IJSE") String name) {
-//        System.out.println("Customer : Instantiated !" + name);
-//    }
 
+    public Customer(@Value("maneesha") String name) {
+        System.out.println(name);
+    }
 
-    public Customer(@Value("Dinithi,Maneesha,gunawardhana,maneesha") String[] names) {
+    public Customer(@Value("Dinithi,Mendis,Maneesha,Gunawardhana") String[] names) {
         for (String name : names) {
             System.out.println(name);
         }
