fa3d9637f26307157051557213fc02ea5514fc30
mGunawardhana <mrgunawardhana27368@gmail.com>
2023-01-15 01:51:35 +0530

    content update

--- SpringFramework/spring_environment/src/lk/ijse/spring/pojp/DBConnection.java
+++ SpringFramework/spring_environment/src/lk/ijse/spring/pojp/DBConnection.java
@@ -1,11 +1,22 @@
 package lk.ijse.spring.pojp;
 
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 
 @Component
-public class DBConnection {
+public class DBConnection implements InitializingBean {
+
+    @Value("${OS}")
+    private String myOSName;
+
     public DBConnection() {
         System.out.println("DBConnection : Instantiated !");
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println(myOSName);
     }
 }
