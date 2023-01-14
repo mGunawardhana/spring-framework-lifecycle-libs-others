8f77eef2a39c20016106e1524062c2b38bc0abf2
mGunawardhana <mrgunawardhana27368@gmail.com>
2023-01-15 00:11:22 +0530

    introducing spring environment

--- SpringFramework/runtime_value_injection/src/lk/ijse/spring/pojo/Customer.java
+++ SpringFramework/runtime_value_injection/src/lk/ijse/spring/pojo/Customer.java
@@ -1,5 +1,6 @@
 package lk.ijse.spring.pojo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
@@ -7,10 +8,12 @@ import org.springframework.stereotype.Component;
 @Component
 public class Customer {
 
+    @Autowired(required = false)
     public Customer(@Value("maneesha") String name) {
         System.out.println(name);
     }
 
+    @Autowired(required = false)
     public Customer(@Value("Dinithi,Mendis,Maneesha,Gunawardhana") String[] names) {
         for (String name : names) {
             System.out.println(name);
---
+++ SpringFramework/runtime_value_injection/src/lk/ijse/spring/pojo/Item.java
@@ -0,0 +1,23 @@
+package lk.ijse.spring.pojo;
+
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Item implements InitializingBean {
+
+    @Value("Welcome")
+    private String name;
+
+    public Item() {
+        System.out.println("Item : Instantiated !");
+        System.out.println(name);
+    }
+
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println(name);
+    }
+}
