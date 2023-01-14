36363019cf35f23fc60dd08ca5c6bc4e1ebb3364
mGunawardhana <mrgunawardhana27368@gmail.com>
2023-01-15 01:14:43 +0530

    getting os environment variables and Java nvironmet variables

--- SpringFramework/spring_environment/src/AppInitializer.java
+++ SpringFramework/spring_environment/src/AppInitializer.java
@@ -1,19 +1,31 @@
+import lk.ijse.spring.config.AppConfig;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import java.util.Map;
+import java.util.Properties;
 
 public class AppInitializer {
     public static void main(String[] args) {
         AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext();
-        ctx.register();
+        ctx.register(AppConfig.class);
         ctx.refresh();
         ctx.registerShutdownHook();
 
 
+        //getting OS environment variable
         Map<String, String> getenv = System.getenv();
-        for (String s:getenv.keySet()) {
+        for (String s : getenv.keySet()) {
             String val = getenv.get(s);
-            System.out.println(val);
+            System.out.println(s + " " + val);
+        }
+
+        System.out.println("================================");
+
+        //Java Environment Variable
+        Properties properties = System.getProperties();
+        for (Object key : properties.keySet()) {
+            Object value = properties.get(key);
+            System.out.println(key + " " + value);
         }
 
     }
