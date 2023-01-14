1a63659635d96ffa5f9cdb934e14ea4be6ed633d
mGunawardhana <mrgunawardhana27368@gmail.com>
2023-01-14 22:52:20 +0530

    using @value annotation

--- SpringFramework/runtime_value_injection/src/lk/ijse/spring/config/AppConfig.java
+++ SpringFramework/runtime_value_injection/src/lk/ijse/spring/config/AppConfig.java
@@ -1,10 +1,18 @@
 package lk.ijse.spring.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan(basePackages = "lk.ijse.spring")
 public class AppConfig {
+
+//    @Bean
+//    public String setD(){
+//        return "string is here !";
+//    }
+
+
 
 }
--- SpringFramework/runtime_value_injection/src/lk/ijse/spring/pojo/Customer.java
+++ SpringFramework/runtime_value_injection/src/lk/ijse/spring/pojo/Customer.java
@@ -1,12 +1,19 @@
 package lk.ijse.spring.pojo;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 
 @Component
 public class Customer {
-    public Customer(String name) {
-        System.out.println("Customer : Instantiated !");
+//    public Customer(@Value("IJSE") String name) {
+//        System.out.println("Customer : Instantiated !" + name);
+//    }
+
+
+    public Customer(@Value("Dinithi,Maneesha,gunawardhana,maneesha") String[] names) {
+        for (String name : names) {
+            System.out.println(name);
+        }
     }
 }
