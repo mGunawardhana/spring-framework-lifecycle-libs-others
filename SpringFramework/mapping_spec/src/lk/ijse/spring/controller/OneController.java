013a555db11daa22b261ebe62e36b298cd5851ef
Maneesha Gunawardhana <100486080+mGunawardhana@users.noreply.github.com>
2023-01-22 23:53:40 +0530

    content update

---
+++ SpringFramework/mapping_spec/src/lk/ijse/spring/controller/OneController.java
@@ -0,0 +1,11 @@
+package lk.ijse.spring.controller;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/one")
+public class OneController {
+
+}
