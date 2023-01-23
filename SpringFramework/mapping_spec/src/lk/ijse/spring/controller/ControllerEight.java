81c0ce92e07e9014a6f08ea5a03747185299f6ff
Maneesha Gunawardhana <100486080+mGunawardhana@users.noreply.github.com>
2023-01-24 00:24:47 +0530

    content update

---
+++ SpringFramework/mapping_spec/src/lk/ijse/spring/controller/ControllerEight.java
@@ -0,0 +1,25 @@
+package lk.ijse.spring.controller;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.awt.*;
+
+@RestController
+@RequestMapping(path = "/eight")
+public class ControllerEight {
+
+    @GetMapping()
+    public String testOne() {
+        return "test 1";
+    }
+
+    @GetMapping(consumes = {})
+    public String testTwo() {
+        return "test 2 - ";
+    }
+
+}
