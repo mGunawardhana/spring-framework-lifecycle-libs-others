e3ab7821eb46688dcbc8fdbcb8cc524442d29a7c
Maneesha Gunawardhana <100486080+mGunawardhana@users.noreply.github.com>
2023-01-23 18:49:41 +0530

    character mapping testing

--- SpringFramework/mapping_spec/src/lk/ijse/spring/controller/ControllerFour.java
+++ SpringFramework/mapping_spec/src/lk/ijse/spring/controller/ControllerFour.java
@@ -1,4 +1,29 @@
 package lk.ijse.spring.controller;
 
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/four")
 public class ControllerFour {
+
+    /*
+
+     Character mapping
+     http://localhost:8080/mapping_spec/C1DEF
+
+     */
+
+    @GetMapping(path = "/C?DEF")
+    public String testOne(){
+        return "character mapping invoked !";
+    }
+
+    @GetMapping(path = "/C?????DEF")
+    public String testOne_A(){
+        return "character test_one_A mapping invoked !";
+    }
+
 }
