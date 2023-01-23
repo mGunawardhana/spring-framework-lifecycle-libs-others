069abaf293039d76e0d8a9a91936b10a78148241
Maneesha Gunawardhana <100486080+mGunawardhana@users.noreply.github.com>
2023-01-23 21:48:32 +0530

    introducing controller seven

--- SpringFramework/mapping_spec/src/lk/ijse/spring/controller/ControllerFive.java
+++ SpringFramework/mapping_spec/src/lk/ijse/spring/controller/ControllerFive.java
@@ -1,4 +1,22 @@
 package lk.ijse.spring.controller;
 
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/five")
 public class ControllerFive {
+
+    @GetMapping(path = "/path/*/A/B")
+    public String testOne(){
+        return "test 1";
+    }
+
+    @GetMapping(path = "/**/path/A/C")
+    public String testOne_A(){
+        return "test 2 !";
+    }
+
 }
