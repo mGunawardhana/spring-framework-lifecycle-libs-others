1f45ca9e295af8b9bfdf7eed9b96108e29a5019c
Maneesha Gunawardhana <100486080+mGunawardhana@users.noreply.github.com>
2023-01-23 11:32:21 +0530

    changing validations using same mapping methord

--- SpringFramework/mapping_spec/src/lk/ijse/spring/controller/ControllerThree.java
+++ SpringFramework/mapping_spec/src/lk/ijse/spring/controller/ControllerThree.java
@@ -2,24 +2,35 @@ package lk.ijse.spring.controller;
 
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/three")
 public class ControllerThree {
-    @GetMapping
-    public String testOne() {
-        return "get method invoked";
+
+    //http://localhost:8080/mapping_spec/three/id/C002/Maneesha
+
+/*
+    @GetMapping(path = "/id/{C001}/{gunawardhana}")
+    public String testOne(@PathVariable("C001") String id, @PathVariable("gunawardhana") String name) {
+        System.out.println(id + " " + name);
+        return "get method invoked - controller 3 id: " + id + " name: " + name;
+    }
+*/
+
+    @GetMapping(path = "/id/{id:[C]{1}[0-9]{3}}/{name}")
+    public String testOne(@PathVariable String id, @PathVariable String name) {
+        System.out.println(id + " " + name);
+        return "get method invoked - controller 3 id: " + id + " name: " + name;
     }
 
-    @GetMapping(path = "/a/b")
-    public String testTwo() {
-        return "get method invoked by method two";
+    @GetMapping(path = "/id/{id:[C]{2}[0-9]{2}}/{name}")
+    public String tesTwo(@PathVariable String id, @PathVariable String name) {
+        System.out.println(id + " " + name);
+        return "get method two invoked - controller 3 id: " + id + " name: " + name;
     }
 
-    @GetMapping(path = "/b")
-    public String testThree() {
-        return "get method invoked by method Three";
-    }
+
 }
