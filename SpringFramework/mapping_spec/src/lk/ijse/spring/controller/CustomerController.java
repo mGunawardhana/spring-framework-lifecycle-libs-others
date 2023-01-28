142bad8ccfadd9eb1ca5b1767c27ac7d4da21ad0
Maneesha Gunawardhana <100486080+mGunawardhana@users.noreply.github.com>
2023-01-28 22:18:22 +0530

    introducing customer dto

--- SpringFramework/mapping_spec/src/lk/ijse/spring/controller/CustomerController.java
+++ SpringFramework/mapping_spec/src/lk/ijse/spring/controller/CustomerController.java
@@ -1,6 +1,5 @@
 package lk.ijse.spring.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,7 +9,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class CustomerController {
 
     @PostMapping
-    public void saveCustomer(){
+    public void saveCustomer() {
+        System.out.println("id " + id + " name " + name + " address " + address + " contact " + contact);
         System.out.println("Wade Goda");
     }
 
