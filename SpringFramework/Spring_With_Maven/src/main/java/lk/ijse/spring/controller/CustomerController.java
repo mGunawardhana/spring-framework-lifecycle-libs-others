b66b5ac5fa516aef53a99d9628c5e36fdbabd9c7
Maneesha Gunawardhana <100486080+mGunawardhana@users.noreply.github.com>
2023-01-29 10:16:21 +0530

    addind servlet api

---
+++ SpringFramework/Spring_With_Maven/src/main/java/lk/ijse/spring/controller/CustomerController.java
@@ -0,0 +1,18 @@
+package lk.ijse.spring.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/customer")
+public class CustomerController {
+
+    @GetMapping
+    public void getCustomer(){
+        System.out.println("getCustomer called");
+    }
+
+}
