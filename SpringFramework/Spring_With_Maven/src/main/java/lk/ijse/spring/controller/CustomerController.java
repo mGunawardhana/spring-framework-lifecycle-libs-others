bb7278d82dd34a7f92d5a1bd733f906d7fbc2c99
Maneesha Gunawardhana <100486080+mGunawardhana@users.noreply.github.com>
2023-01-29 13:31:11 +0530

    content update

--- SpringFramework/Spring_With_Maven/src/main/java/lk/ijse/spring/controller/CustomerController.java
+++ SpringFramework/Spring_With_Maven/src/main/java/lk/ijse/spring/controller/CustomerController.java
@@ -1,6 +1,7 @@
 package lk.ijse.spring.controller;
 
 import lk.ijse.spring.dto.CustomerDTO;
+import lk.ijse.spring.util.ResponseUtil;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -8,9 +9,16 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/customer")
 public class CustomerController {
 
+/*    @PostMapping
+    public ResponseUtil saveCustomer(@ModelAttribute CustomerDTO customerDTO){
+        System.out.println(customerDTO.toString());
+        return new ResponseUtil("OK","Success fully Registered !",null);
+    }*/
+
     @PostMapping
-    public void saveCustomer(@ModelAttribute CustomerDTO customerDTO){
+    public void saveCustomer(CustomerDTO customerDTO){
         System.out.println(customerDTO.toString());
     }
+
 
 }
