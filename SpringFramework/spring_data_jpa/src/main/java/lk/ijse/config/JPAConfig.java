e885d1c847c4a1673bb6b9adf0f1cfbdb68d50ff
Maneesha Gunawardhana <100486080+mGunawardhana@users.noreply.github.com>
2023-02-04 11:47:40 +0530

    introducing data source

--- SpringFramework/spring_data_jpa/src/main/java/lk/ijse/config/JPAConfig.java
+++ SpringFramework/spring_data_jpa/src/main/java/lk/ijse/config/JPAConfig.java
@@ -1,4 +1,32 @@
 package lk.ijse.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+
+import javax.sql.DataSource;
+
+@Configuration
 public class JPAConfig {
+
+    @Autowired
+    private DataSource ds;
+
+    /*introducing LocalContainerEntityManagerFactoryBean in the parent context */
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource ds, JpaVendorAdapter va) {
+        LocalContainerEntityManagerFactoryBean factoryBean = new LocalContainerEntityManagerFactoryBean();
+        factoryBean.setPackagesToScan("lk.ijse.entity");
+        factoryBean.setDataSource(ds);
+        factoryBean.setJpaVendorAdapter(va);
+        return factoryBean;
+    }
+
+    @Bean
+    public DataSource dataSource(){
+
+    }
+
 }
