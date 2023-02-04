62c13198f4375c8aebe8b5b152a945dc2af83abd
Maneesha Gunawardhana <100486080+mGunawardhana@users.noreply.github.com>
2023-02-04 13:57:47 +0530

    content update

--- SpringFramework/spring_data_jpa/src/main/java/lk/ijse/config/JPAConfig.java
+++ SpringFramework/spring_data_jpa/src/main/java/lk/ijse/config/JPAConfig.java
@@ -3,11 +3,14 @@ package lk.ijse.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.JpaVendorAdapter;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.Database;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
 
+import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 
 @Configuration
@@ -29,7 +32,7 @@ public class JPAConfig {
     public DataSource dataSource() {
         DriverManagerDataSource ds = new DriverManagerDataSource();
         ds.setDriverClassName("com.mysql.cj.jdbc.Driver");
-        ds.setUrl("jdbc:mysql://localhost:3306/data_jpa?createDatabaseIfNotExists=true");
+        ds.setUrl("jdbc:mysql://localhost:3306/spring?createDatabaseIfNotExist=true");
         ds.setUsername("root");
         ds.setPassword("1234");
         return ds;
@@ -38,11 +41,16 @@ public class JPAConfig {
     @Bean
     public JpaVendorAdapter jpaVendorAdapter() {
         HibernateJpaVendorAdapter jpaVendorAdapter = new HibernateJpaVendorAdapter();
+        jpaVendorAdapter.setDatabasePlatform("org.hibernate.dialect.MySQL8Dialect");
         jpaVendorAdapter.setDatabase(Database.MYSQL);
         jpaVendorAdapter.setGenerateDdl(true);
-        jpaVendorAdapter.setDatabasePlatform("org.hibernate.dialect.MySQL8Dialect");
         jpaVendorAdapter.setShowSql(true);
         return jpaVendorAdapter;
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager(EntityManagerFactory emf) {
+        return new JpaTransactionManager(emf);
     }
 
 }
