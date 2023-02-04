726f33b403d49d99b08609bdd8559158f2a0c385
Maneesha Gunawardhana <100486080+mGunawardhana@users.noreply.github.com>
2023-02-04 09:52:13 +0530

    content update

--- SpringFramework/spring_data_jpa/src/main/java/lk/ijse/entity/Customer.java
+++ SpringFramework/spring_data_jpa/src/main/java/lk/ijse/entity/Customer.java
@@ -1,4 +1,28 @@
 package lk.ijse.entity;
 
+import lombok.*;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+@ToString
+@EqualsAndHashCode
 public class Customer {
+    @Id
+/*
+    @Column(nullable = false,length = 45)
+*/
+    private String id;
+    @Column(nullable = false,length = 45)
+    private String name;
+    @Column(nullable = false,length = 45)
+    private String address;
+    @Column(nullable = false,length = 45)
+    private String contact;
 }
--- SpringFramework/spring_data_jpa/src/main/java/lk/ijse/entity/Item.java
+++ SpringFramework/spring_data_jpa/src/main/java/lk/ijse/entity/Item.java
@@ -1,4 +1,27 @@
 package lk.ijse.entity;
 
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+@ToString
+@EqualsAndHashCode
 public class Item {
+
+    @Id
+/*
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+*/
+    private String itemId;
+    @Column(nullable = false,length = 45)
+    private String itemName;
+    @Column(nullable = false,length = 45)
+    private int qty;
+    @Column(nullable = false,length = 45)
+    private double unitPrice;
 }
