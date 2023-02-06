c13e8826eec3d48b1a15c5a742e6cab96d20d030
mGunawardhana <mrgunawardhana27368@gmail.com>
2023-02-06 20:27:44 +0530

    introducng custpmer implementation

--- SpringFramework/spring_data_jpa/src/main/java/lk/ijse/service/CustomerService.java
+++ SpringFramework/spring_data_jpa/src/main/java/lk/ijse/service/CustomerService.java
@@ -1,4 +1,13 @@
 package lk.ijse.service;
 
+import lk.ijse.dto.CustomerDTO;
+
+import java.util.ArrayList;
+
 public interface CustomerService {
+    public void saveCustomer(CustomerDTO customerDTO);
+    public void updateCustomer(CustomerDTO customerDTO);
+    public void deleteCustomer(String id);
+    public ArrayList<CustomerDTO> getAllCustomers();
+
 }
---
+++ SpringFramework/spring_data_jpa/src/main/java/lk/ijse/service/impl/CustomerServiceImpl.java
@@ -0,0 +1,28 @@
+package lk.ijse.service.impl;
+
+import lk.ijse.dto.CustomerDTO;
+import lk.ijse.service.CustomerService;
+
+import java.util.ArrayList;
+
+public class CustomerServiceImpl implements CustomerService {
+    @Override
+    public void saveCustomer(CustomerDTO customerDTO) {
+
+    }
+
+    @Override
+    public void updateCustomer(CustomerDTO customerDTO) {
+
+    }
+
+    @Override
+    public void deleteCustomer(String id) {
+
+    }
+
+    @Override
+    public ArrayList<CustomerDTO> getAllCustomers() {
+        return null;
+    }
+}
