5a5846b34e58e955ab82f3a9942b99a2822533b9
mGunawardhana <mrgunawardhana27368@gmail.com>
2023-02-06 21:23:27 +0530

    introducing placeorderserviceimplementations form

--- SpringFramework/spring_data_jpa/src/main/java/lk/ijse/service/PlaceOrderService.java
+++ SpringFramework/spring_data_jpa/src/main/java/lk/ijse/service/PlaceOrderService.java
@@ -1,4 +1,21 @@
 package lk.ijse.service;
 
+import lk.ijse.dto.CustomerDTO;
+import lk.ijse.dto.ItemDTO;
+import lk.ijse.dto.OrderDTO;
+import lk.ijse.dto.OrderDetailsDTO;
+
+import java.util.ArrayList;
+
 public interface PlaceOrderService {
+    public ArrayList<CustomerDTO> loadAllCustomersInTheCombo();
+
+    public ArrayList<ItemDTO> loadAllItemsInTheCombo();
+
+    public ArrayList<OrderDetailsDTO> getAllOrderDetails();
+
+    public ArrayList<OrderDTO> getAllOrders();
+
+    public void manageTransAction();
+
 }
---
+++ SpringFramework/spring_data_jpa/src/main/java/lk/ijse/service/impl/PlaceOrderServiceImpl.java
@@ -0,0 +1,36 @@
+package lk.ijse.service.impl;
+
+import lk.ijse.dto.CustomerDTO;
+import lk.ijse.dto.ItemDTO;
+import lk.ijse.dto.OrderDTO;
+import lk.ijse.dto.OrderDetailsDTO;
+import lk.ijse.service.PlaceOrderService;
+
+import java.util.ArrayList;
+
+public class PlaceOrderServiceImpl implements PlaceOrderService {
+    @Override
+    public ArrayList<CustomerDTO> loadAllCustomersInTheCombo() {
+        return null;
+    }
+
+    @Override
+    public ArrayList<ItemDTO> loadAllItemsInTheCombo() {
+        return null;
+    }
+
+    @Override
+    public ArrayList<OrderDetailsDTO> getAllOrderDetails() {
+        return null;
+    }
+
+    @Override
+    public ArrayList<OrderDTO> getAllOrders() {
+        return null;
+    }
+
+    @Override
+    public void manageTransAction() {
+
+    }
+}
