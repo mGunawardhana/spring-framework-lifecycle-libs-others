33a76d327a455c8d4a5c5ea173db8639161fc757
mGunawardhana <mrgunawardhana27368@gmail.com>
2023-02-06 20:29:41 +0530

    introducing itemservice implementaion

--- SpringFramework/spring_data_jpa/src/main/java/lk/ijse/service/ItemService.java
+++ SpringFramework/spring_data_jpa/src/main/java/lk/ijse/service/ItemService.java
@@ -1,4 +1,13 @@
 package lk.ijse.service;
 
+import lk.ijse.dto.CustomerDTO;
+import lk.ijse.dto.ItemDTO;
+
+import java.util.ArrayList;
+
 public interface ItemService {
+    public void saveCustomer(ItemDTO itemDTO);
+    public void updateCustomer(ItemDTO itemDTO);
+    public void deleteCustomer(String id);
+    public ArrayList<ItemDTO> getAllCustomers();
 }
---
+++ SpringFramework/spring_data_jpa/src/main/java/lk/ijse/service/impl/ItemServiceImpl.java
@@ -0,0 +1,28 @@
+package lk.ijse.service.impl;
+
+import lk.ijse.dto.ItemDTO;
+import lk.ijse.service.ItemService;
+
+import java.util.ArrayList;
+
+public class ItemServiceImpl implements ItemService {
+    @Override
+    public void saveCustomer(ItemDTO itemDTO) {
+
+    }
+
+    @Override
+    public void updateCustomer(ItemDTO itemDTO) {
+
+    }
+
+    @Override
+    public void deleteCustomer(String id) {
+
+    }
+
+    @Override
+    public ArrayList<ItemDTO> getAllCustomers() {
+        return null;
+    }
+}
