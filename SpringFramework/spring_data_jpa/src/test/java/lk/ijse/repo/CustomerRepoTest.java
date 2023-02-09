9405c6c30f9e83e9d618b3763d7987404b79fa29
mGunawardhana <mrgunawardhana27368@gmail.com>
2023-02-09 09:25:23 +0530

    content update

--- SpringFramework/spring_data_jpa/src/test/java/lk/ijse/repo/CustomerRepoTest.java
+++ SpringFramework/spring_data_jpa/src/test/java/lk/ijse/repo/CustomerRepoTest.java
@@ -1,15 +1,13 @@
 package lk.ijse.repo;
 
 import lk.ijse.config.WebRootConfig;
-import org.junit.jupiter.api.AfterAll;
+import lk.ijse.entity.Customer;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.web.WebAppConfiguration;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 @WebAppConfiguration
 @ContextConfiguration(classes = {WebRootConfig.class})
@@ -21,6 +19,7 @@ class CustomerRepoTest {
 
     @Test
     void findCustomerByName() {
-
+        Customer name = customerRepo.findCustomerByName("maneesha");
+        System.out.println(name.toString());
     }
 }
